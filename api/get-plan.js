2fc68b7545b2091386fdad2b1a9ef5f5754f821c
Emily Sas <emilysas@users.noreply.github.com>
2025-07-10 15:21:00 +0100

    Update get-plan.js

--- api/get-plan.js
+++ api/get-plan.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   const { planId } = req.query;
   if (!planId) {
     return res.status(400).json({ error: "Missing planId" });
